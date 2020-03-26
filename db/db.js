a59944a61b07292be8944b4daa713cce430c5375
YOUOHSHIMA <youbinoyou@gmail.xom>
2020-03-27 04:07:23 +0900

    基礎ページ作成

---
+++ db/db.js
@@ -0,0 +1,243 @@
+var sideNavMenus = [
+  /// シェアプログラミング start
+  {
+    header : {
+      title : "シェアプログラミング",
+      img : "school.png",
+    },
+    items : [
+      {
+        title : "Howto",
+        href : "howto.html",
+        img : "howto_icon.png",
+      },
+    ],
+  },
+  /// シェアプログラミング end
+
+  /// 開発について start
+  {
+    header : {
+      title : "開発について",
+      img : "school.png",
+    },
+    items : [
+      // Git
+      {
+        title : "Git",
+        href : "git.html",
+        img : "howto_icon.png",
+        hidden:true,
+      },
+    ],
+  },
+  /// 開発について end
+
+  /// Front Side start
+  {
+    header : {
+      title : "Front Side",
+      img : "school.png",
+    },
+    items : [
+      /// HTML
+      {
+        title : "HTML",
+        href : "html.html",
+        img : "html_icon.png",
+      },
+      /// CSS
+      {
+        title : "CSS",
+        href : "css.html",
+        img : "css_icon.png",
+        hidden:true,
+      },
+      /// Bootstrap
+      {
+        title : "Bootstrap",
+        href : "bootstrap.html",
+        img : "bootstrap_icon.png",
+        hidden:true,
+      },
+      /// Colors
+      {
+        title : "Colors",
+        href : "colors.html",
+        img : "colors_icon.png",
+        hidden:true,
+      },
+      /// Icons
+      {
+        title : "Icons",
+        href : "icons.html",
+        img : "icons_icon.png",
+        hidden:true,
+      },
+      /// Graphics
+      {
+        title : "Graphics",
+        href : "graphics.html",
+        img : "graphics_icon.png",
+        hidden:true,
+      },
+      /// Sass
+      {
+        title : "Sass",
+        href : "sass.html",
+        img : "sass_icon.png",
+        hidden:true,
+      },
+    ],
+  },
+  /// Front Side end
+  /// JavaScript start
+  {
+    header : {
+      title : "JavaScript",
+      img : "school.png",
+    },
+    items : [
+      /// JavaScript
+      {
+        title : "JavaScript",
+        href : "javascript.html",
+        img : "javascript_icon.png",
+        hidden:true,
+      },
+      /// jQuery
+      {
+        title : "jQuery",
+        href : "jquery.html",
+        img : "jquery_icon.png",
+        hidden:true,
+      },
+      /// JSON
+      {
+        title : "JSON",
+        href : "json.html",
+        img : "json_icon.png",
+        hidden:true,
+      },
+    ],
+  },
+  /// JavaScript end
+  /// Server Side start
+  {
+    header : {
+      title : "Server Side",
+      img : "school.png",
+    },
+    items : [
+      /// PHP
+      {
+        title : "PHP",
+        href : "php.html",
+        img : "php_icon.png",
+        hidden:true,
+      },
+      /// SQL
+      {
+        title : "SQL",
+        href : "sql.html",
+        img : "sql_icon.png",
+        hidden:true,
+      },
+      /// Python
+      {
+        title : "Python",
+        href : "python.html",
+        img : "python_icon.png",
+        hidden:true,
+      },
+      /// Python
+      {
+        title : "Ruby",
+        href : "ruby.html",
+        img : "ruby_icon.png",
+        hidden:true,
+      },
+    ],
+  },
+  /// Server Side end
+  /// iOS start
+  {
+    header : {
+      title : "iOS",
+      img : "school.png",
+    },
+    items : [
+      // Objective-C（iOS）
+      {
+        title : "Objective-C（iOS）",
+        href : "iOS/objectivec.html",
+        img : "ios_objectivec_icon.png",
+        hidden:true,
+      },
+      // Swift（iOS）
+      {
+        title : "Swift（iOS）",
+        href : "iOS/swift.html",
+        img : "ios_swift_icon.png",
+        hidden:true,
+      },
+      // ShellScript
+      {
+        title : "ShellScript",
+        href : "shellscript.html",
+        img : "ios_shellscript_icon.png",
+        hidden:true,
+      },
+
+    ],
+  },
+  /// iOS end
+  /// Android start
+  {
+    header : {
+      title : "Android",
+      img : "school.png",
+    },
+    items : [
+      // Java（Android）
+      {
+        title : "Java（Android）",
+        href : "Android/java.html",
+        img : "android_java_icon.png",
+        hidden:true,
+      },
+    ],
+  },
+  /// Android end
+  /// Programming start
+  {
+    header : {
+      title : "Programming",
+      img : "school.png",
+    },
+    items : [
+      // C言語
+      {
+        title : "C言語",
+        href : "c.html",
+        img : "c_icon.png",
+        hidden:true,
+      },
+      // C++
+      {
+        title : "C++",
+        href : "cpp.html",
+        img : "cpp_icon.png",
+        hidden:true,
+      },
+      // C#
+      {
+        title : "C#",
+        href : "cs.html",
+        img : "cs_icon.png",
+        hidden:true,
+      },
+    ],
+  },
+  /// Programming end
+];
